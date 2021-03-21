baea55d18aadef8b0b28414a70ced08d94ea637a
Volodymyr Nerovnia <nerv@i.ua>
2021-03-22 01:21:20 +0200

    Changes to be committed: 	modified:   lib/aggregator.js 	modified:   package-lock.json 	modified:   test/aggregator.js

--- test/aggregator.js
+++ test/aggregator.js
@@ -1,3 +1,17 @@
 "use strict";
 
 var Aggregator = require('../index.js');
+
+describe('Aggregator', function() {
+  let parser = new Aggregator();
+  it('should save without error', function(done) {
+    let Parser = require('rss-parser');
+    let parser = new Parser();
+    (async () => {
+      let obj = await parser.parseURL('https://www.liga.net/news/rss.xml');
+      console.log(obj);
+      done()
+    })();
+
+  });
+});
